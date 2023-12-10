66bf4defe7637bd7044ccbdb8c276075242ef491
petialetia <petialetia@yandex.ru>
2023-12-11 00:24:40 +0300

    gtest installation is added to requirements_ubuntu

--- requirements_ubuntu.sh
+++ requirements_ubuntu.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+TARGET="$1"
+START_FOLDER=$(pwd)
+
 apt-get update
 apt-get install -y sudo
 
 sudo apt-get install -y clang
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmake
+
+if [[($@ == *'--test'*)]]
+then
+    sudo apt-get install -y libgtest-dev
+    cd /usr/src/gtest
+    sudo cmake .
+    sudo make
+    cd "$START_FOLDER"
+fi
